fc96bdc0dd92eb3af717bd5a47767691dca7b67a
albyrex <51365351+albyrex@users.noreply.github.com>
2020-12-04 12:03:08 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,7 @@
 
 
 
-window.addEventListener("load", function f() {	    
+window.addEventListener("load", function f() {
     doAjaxPost("api/userStatus.php", [], function(responseText) {
 		let response = JSON.parse(responseText);
 		if(response.body.status != 1) {
@@ -11,7 +11,7 @@ window.addEventListener("load", function f() {
 		}
 
 		search.addEventListener("change", searchBook);
-		
+
 	});
 });
 
@@ -22,30 +22,32 @@ function populateBook(responseText) {
         alert("Error requesting data.\nError message: " + response.body);
         return;
     }
-	
+
 	let listbook = response.body.bookList;
-	
+
 	clearTable(booklist);
-	
+
 	for (var i = 0; i < listbook.length; ++i) {
 		var book = listbook[i];
-		var x = document.createElement("tr"); 
-		var t = document.createElement("td"); 
+		var x = document.createElement("tr");
+		var t = document.createElement("td");
 		var a = document.createElement("td");
 		var p = document.createElement("td");
 		var b = document.createElement("button");
-		
+    var b_td = document.createElement("td");
+    b_td.appendChild(b);
+
 		t.innerText = book.title;
 		a.innerText = book.author;
 		p.innerText = book.price;
 		b.innerText = "Remove";
 		b.setAttribute("idBook", book.id_book);
-		b.addEventListener("click", removeBook);		
+		b.addEventListener("click", removeBook);
 		x.appendChild(t);
 		x.appendChild(a);
 		x.appendChild(p);
-		x.appendChild(b);
-		
+		x.appendChild(b_td);
+
 		booklist.appendChild(x)
 	}
 }
